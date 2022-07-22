6e4561064a8a23f2f76a68a7471828293f7a69b1
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2022-07-22 14:26:39 +0200

    Project barebones
    Added the barebones for the project to work

---
+++ Code/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+	private DiscordSocketClient _client;
+	public static Task Main(string[] args) => new Program().MainAsync();
+
+	public async Task MainAsync()
+	{
+		_client = new DiscordSocketClient();
+		_client.Log += Log;
+
+		var appConfig = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+		var token = appConfig["DiscordBotToken"];
+
+		await _client.LoginAsync(TokenType.Bot, token);
+		await _client.StartAsync();
+
+		// Block this task until the program is closed.
+		await Task.Delay(-1);
+	}
+
+	private Task Log(LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+
+}
